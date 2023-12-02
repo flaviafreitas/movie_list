ade6992171801c2547ba9f6a64a60c69d80a0bb8
Flávia Freitas <flaviarobertasf@gmail.com>
2023-12-02 00:24:32 -0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = (props) => {
+    function handleSubmit(event) {
+      event.preventDefault();
+      const searchValue = event.target[0].value;
+      props.onSubmit(searchValue);
+      event.target[0].value = "";
+    }
+  
+    return (
+      <header className="Header">
+        <h1>ListFlix</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Pesquise por filmes" />
+        </form>
+      </header>
+    );
+  };
+  
+  export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.scss'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home/home.jsx';
+import MovieDetail from './pages/movieDetail/MovieDetail.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "/movie/:id",
+        element: <MovieDetail />
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
